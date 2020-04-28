800a6f27012e24cf6086163ca7388e0b9ec1a9d3
gorynychalex <gorynychalex@gmail.com>
2020-04-28 14:51:18 +1000

    calculator added

---
+++ src/ru/popovich/demo01/Calculator.java
@@ -0,0 +1,8 @@
+package ru.popovich.demo01;
+
+public class Calculator {
+
+    public int add(int a, int b){
+	    return (a+b);
+    }
+}
--- src/ru/popovich/demo01/Main.java
+++ src/ru/popovich/demo01/Main.java
@@ -2,8 +2,17 @@ package ru.popovich.demo01;
 
 public class Main {
     public static void main(String[] args) {
+
+	    int a = 3, b = 4;
+
 	    System.out.println("Hello world");
+
 	    Second second = new Second();
+
 	    System.out.println(second.toString());
+
+	    Calculator calculator = new Calculator();
+
+	    System.out.println("Sum " + a + " + " + b + " = " + calculator.add(a,b));
     }
 }
